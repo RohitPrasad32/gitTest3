03e7481954a7b228d519eb9fcf060188ccacb3b8
prasadrohit532@gmail.com <prasadrohit532@gmail.com>
2025-08-31 23:32:49 +0530

    First commit

---
+++ src/main/java/com/gitTrial/GitTrialApplication.java
@@ -0,0 +1,14 @@
+package com.gitTrial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTrialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitTrialApplication.class, args);
+	}
+ int a = 20;
+	String s = "Hello World!";
+}
---
+++ src/test/java/com/gitTrial/GitTrialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitTrial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTrialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
